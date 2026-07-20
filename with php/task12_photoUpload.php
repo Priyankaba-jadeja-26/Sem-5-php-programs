0798083745dd1e72af1811793e93c5926be11a30
Priyankaba-jadeja-26 <priyankaba2611@gmail.com>
2026-07-20 09:44:09 +0530

    Completed task12: Data of the student is being added to csv file.

--- with php/task12_photoUpload.php
+++ with php/task12_photoUpload.php
@@ -6,31 +6,31 @@
     <title>Student Registration Form</title>
 </head>
 <body>
-    <form action="" method="post" enctype="multipart/form-data"></form>
+    <form action="" method="post" enctype="multipart/form-data">
         <fieldset>
             <legend>Student Details: </legend>
             
             <label for="studID">Student ID</label>
-            <input type="number" name="studID" id="studID">
+            <input type="number" name="studID" id="studID" required>
             <br>
 
             <label for="studName">Name</label>
-            <input type="text" name="studName" id="studName">
+            <input type="text" name="studName" id="studName" required>
             <br><br>
             <label for="gender">Select Your Gender :</label>
             <br>
             <label for="Male">Male</label>
-            <input type="radio" name="Male" id="Male">
+            <input type="radio" name="gender" id="Male" value="Male">
 
             <label for="Female">Female</label>
-            <input type="radio" name="Female" id="Female">
+            <input type="radio" name="gender" id="Female" value="Female">
 
             <label for="Other">Other</label>
-            <input type="radio" name="Other" id="Other">
+            <input type="radio" name="gender" id="Other" value="Other">
             <br><br>
 
             <label for="course">Select Course : </label>
-            <select name="course" id="couse">
+            <select name="course" id="course" required>
                 <option value="">------Select Your Course------</option>
                 <option value="MSC[CA-IT]">MSC[CA-IT]</option>
                 <option value="MSC[IT]">MSC[IT]</option>
@@ -40,11 +40,11 @@
             <br><br>
 
             <label for="mobile">Mobile number</label>
-            <input type="tel" name="mobile" id="mobile">
+            <input type="tel" name="mobile" id="mobile" required>
             <br><br>
 
             <label for="photo">Upload your photo</label>
-            <input type="file" name="photo" id="photoUpload">
+            <input type="file" name="photo" id="photoUpload" required>
             <br>
 
             <br>
@@ -58,8 +58,22 @@
 
 <?php
     echo "PHP Script runned.";
+    
     if(isset($_FILES['photo'])){
+
+        $file = fopen("Task12_StudentData.csv","a");
+        echo "File Opened";
+
         $fileName = $_FILES['photo']['name'];
-        echo "photo set";
+        $fileSize = $_FILES['photo']['size'];
+
+        $destination = __DIR__. "/images_php/" . $fileName;
+        move_uploaded_file($_FILES['photo']['tmp_name'],$destination);
+        $studData = [ "ID" => $_POST['studID'], "Name" => $_POST['studName'], "gender" => $_POST['gender'], "Course" => $_POST['course'], "MobileNo" => $_POST['mobile'], "Photo" => $_FILES['photo']['name']];
+
+        fputcsv($file,$studData);
+        print_r($studData);
+
+        fclose($file);
     }
 ?>
